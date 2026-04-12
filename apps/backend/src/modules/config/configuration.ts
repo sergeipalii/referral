bd205677df3adf6dbefe999fabd0331d030327d4
Sergei Palii <serg.paliy@yandex.ru>
2026-04-12 20:05:19 +0700

    docker setup fix

--- apps/backend/src/modules/config/configuration.ts
+++ apps/backend/src/modules/config/configuration.ts
@@ -3,8 +3,8 @@ import * as process from 'node:process';
 export default () => ({
   database: {
     type: 'postgres',
-    host: 'localhost',
-    port: 5432,
+    host: process.env.DB_HOST || 'localhost',
+    port: parseInt(process.env.DB_PORT || '', 10) || 5432,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
